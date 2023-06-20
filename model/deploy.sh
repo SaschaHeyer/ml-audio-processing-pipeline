a450f62e8e4d5f2a32403ecfe2a76406ef71ae60
Marcin <marcin@doit-intl.com>
2023-06-20 19:50:24 +0200

    Fixing model setup scripts.

--- model/deploy.sh
+++ model/deploy.sh
@@ -1,9 +1,37 @@
-gcloud ai endpoints deploy-model 3960172602411974656 \
-  --project=sascha-playground-doit \
-  --region=us-central1 \
-  --model=6378392297054142464 \
-  --traffic-split=0=100 \
-  --machine-type="n1-standard-16" \
-  --accelerator=type="nvidia-tesla-t4,count=1" \
-  --display-name=image-audio-classification\
-  --service-account='workflow-poc@sascha-playground-doit.iam.gserviceaccount.com'
+PROJECT_ID=$(gcloud config get project)
+
+ENDPOINT_ID=$(gcloud ai endpoints list --project=$PROJECT_ID --region=us-central1 | grep 'workflow-poc-endpoint' | awk '{print $1}')
+
+if [ -z "$ENDPOINT_ID" ]
+then
+	
+	echo "Creating endpoint"
+	
+	gcloud ai endpoints create --project=$PROJECT_ID --region=us-central1 --display-name=workflow-poc-endpoint
+	
+	ENDPOINT_ID=$(gcloud ai endpoints list --project=$PROJECT_ID --region=us-central1 | grep 'workflow-poc-endpoint' | awk '{print $1}')
+
+	echo "Endpoint $ENDPOINT_ID created"
+else
+	echo "Endpoint already exists, deploying model"
+fi
+
+MODEL_ID=$(gcloud ai models list --project=$PROJECT_ID --region=us-central1 | grep 'audio-classification' | awk '{print $1}')
+
+echo "Deploying model $MODEL_ID"
+
+if [ -z "$ENDPOINT_ID" ] || [ -z "$MODEL_ID" ]
+then
+      echo "ERROR: Endpoint or model ID is empty"
+else
+      gcloud ai endpoints deploy-model $ENDPOINT_ID \
+	  --project=$PROJECT_ID \
+	  --region=us-central1 \
+	  --model=$MODEL_ID \
+	  --traffic-split=0=100 \
+	  --machine-type="n1-standard-16" \
+	  --accelerator=type="nvidia-tesla-t4,count=1" \
+	  --display-name=image-audio-classification\
+	  --service-account="workflow-poc@$PROJECT_ID.iam.gserviceaccount.com"
+fi
+
--- model/upload.sh
+++ model/upload.sh
@@ -1,7 +1,9 @@
+PROJECT_ID=(gcloud config get project_id)
+
 gcloud ai models upload \
   --container-ports=8080 \
   --container-predict-route="/predict" \
   --container-health-route="/health" \
   --region=us-central1 \
   --display-name=audio-classification \
-  --container-image-uri=gcr.io/sascha-playground-doit/audio-classification
+  --container-image-uri=gcr.io/$PROJECT_ID/audio-classification
